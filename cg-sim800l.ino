76c6667d6d0bd5ae5d2370b9d163c194e0a70742
Md. Rashadul Alam <rashed.droid@gmail.com>
2018-12-30 00:20:11 +0600

    cg-sim800l: added at command doc

--- cg-sim800l.ino
+++ cg-sim800l.ino
@@ -1,35 +1,100 @@
-/*
-Sketch: SIM800 Serial Debug 
-
-Function: This sketch is used to communicate with SIM800 with AT commands.
-*********************************************************************************
-note: the following pins has been used and should not be used for other purposes.
-  pin 8   // tx pin
-  pin 7   // rx pin
-  pin 9   // power key pin
-  pin 12  // power status pin
-*********************************************************************************
-created on 2013/12/5, version: 0.1
-by lawliet.zou(lawliet.zou@gmail.com)
-*/
-
-#include "gprs.h"
 #include <SoftwareSerial.h>
 
-GPRS gprs(9600);//BaudRate
+SoftwareSerial gsm(10, 11);
 
-void setup(){
-  Serial.begin(9600);
-  while(!Serial);
-  Serial.println("Serial debug begin...");
-  gprs.preInit();//Power on SIM800
-  while(0 != gprs.sendATTest())
-  {
-      Serial.println("waiting for sim800 initialization!!");
-  }
-  Serial.println("sim800 init O.K!");
+void setup() {
+   Serial.begin(9600);
+
+   while(!Serial);
+
+   gsm.begin(9600);
+   delay(1000);
+
+   Serial.println("Ready to receive commands");
 }
 
-void loop(){
-  gprs.serialDebug();
+void loop() {
+   if(gsm.available()){
+     Serial.write(gsm.read());
+   }
+
+   if(Serial.available()){
+     gsm.write(Serial.read());
+   }
 }
+
+
+
+
+//#include <SoftwareSerial.h>
+//SoftwareSerial sim(10, 11);
+//int _timeout;
+//String _buffer;
+//String number = "+8801794620787"; //-> change with your number
+//void setup() {
+//  delay(7000); //delay for 7 seconds to make sure the modules get the signal
+//  Serial.begin(9600);
+//  _buffer.reserve(50);
+//  Serial.println("Sistem Started...");
+//  sim.begin(9600);
+//  delay(1000);
+//  Serial.println("Type s to send an SMS, r to receive an SMS, and c to make a call");
+//}
+//void loop() {
+//  if (Serial.available() > 0)
+//    switch (Serial.read())
+//    {
+//      case 's':
+//        SendMessage();
+//        break;
+//      case 'r':
+//        RecieveMessage();
+//        break;
+//      case 'c':
+//        callNumber();
+//        break;
+//    }
+//  if (sim.available() > 0)
+//    Serial.write(sim.read());
+//}
+//void SendMessage()
+//{
+//  //Serial.println ("Sending Message");
+//  sim.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
+//  delay(1000);
+//  //Serial.println ("Set SMS Number");
+//  sim.println("AT+CMGS=\"" + number + "\"\r"); //Mobile phone number to send message
+//  delay(1000);
+//  String SMS = "Hello, how are you?";
+//  sim.println(SMS);
+//  delay(100);
+//  sim.println((char)26);// ASCII code of CTRL+Z
+//  delay(1000);
+//  _buffer = _readSerial();
+//}
+//void RecieveMessage()
+//{
+//  Serial.println ("SIM800L Read an SMS");
+//  delay (1000);
+//  sim.println("AT+CNMI=2,2,0,0,0"); // AT Command to receive a live SMS
+//  delay(1000);
+//  Serial.write ("Unread Message done");
+//}
+//String _readSerial() {
+//  _timeout = 0;
+//  while  (!sim.available() && _timeout < 12000  )
+//  {
+//    delay(13);
+//    _timeout++;
+//  }
+//  if (sim.available()) {
+//    return sim.readString();
+//  }
+//}
+//void callNumber() {
+//  sim.print (F("ATD"));
+//  sim.print (number);
+//  sim.print (F(";\r\n"));
+//  _buffer = _readSerial();
+//  Serial.println(_buffer);
+//}
